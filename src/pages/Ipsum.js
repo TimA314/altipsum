0b4bc5b22b508e03ae9ced522400a2b9482691df
TimA314 <ci0d7j9m@mailer.me>
2021-12-29 15:28:46 -0800

    Add shakespeare ipsum

--- src/pages/Ipsum.js
+++ src/pages/Ipsum.js
@@ -1,31 +1,55 @@
 import React from "react";
 import { useState } from "react";
 import IpsumText from "../Components/IpsumText";
+import { getLorem } from "../services/loremService";
 import { getNews } from "../services/newsService";
+import { getShakespeare } from "../services/shakespeare";
 import "./Ipsum.css";
 
 export default function Ipsum() {
-  const [newsData, setNewsData] = useState();
+  const [text, setText] = useState();
 
   function handleNewsGenerate(e) {
     e.preventDefault();
     getNews()
       .then((res) => {
         let data = res.data.articles.map(({ description }) => description);
-        setNewsData(data);
+        setText(data);
       })
       .catch((err) => console.log(err));
   }
+
+  function handleShakespeareGenerate(e) {
+    e.preventDefault();
+    setText(getShakespeare());
+  }
+
+  function handleLoremGenerate(e) {
+    e.preventDefault();
+    setText(getLorem());
+  }
+
+  const copy = async () => {
+    await navigator.clipboard.writeText(text);
+    alert("Text copied");
+  };
 
   return (
     <div className="background">
       <h1>Ipsum</h1>
       <p>Generate Ipsum of your choice</p>
+      <button onClick={copy}>Copy</button>
       <br />
+      <button type="submit" className="btn" onClick={handleLoremGenerate}>
+        Generate Lorem
+      </button>
+      <button type="submit" className="btn" onClick={handleShakespeareGenerate}>
+        Generate Shakespeare
+      </button>
       <button type="submit" className="btn" onClick={handleNewsGenerate}>
         Generate News
       </button>
-      <IpsumText IpsumText={newsData} />
+      <IpsumText IpsumText={text} />
     </div>
   );
 }
---
+++ src/services/shakespeare.js
@@ -0,0 +1,9 @@
+const getShakespeare = () => {
+  const text = [
+    "Truth may seem but cannot be; Beauty brag but tis not she; Truth and beauty buried be. When they had sworn to this advised doom, They did conclude to bear dead Lucrece thence; To show her bleeding body thorough Rome, And so to publish Tarquins foul offence: Which being done with speedy diligence, The Romans plausibly did give consent To Tarquins everlasting banishment. A womans face with Natures own hand painted Hast thou, the master-mistress of my passion; A womans gentle heart, but not acquainted With shifting change, as is false womens fashion; From fairest creatures we desire increase, That thereby beautys rose might never die, But as the riper should by time decease, His tender heir might bear his memory: In me thou seest the glowing of such fire, That on the ashes of his youth doth lie, As the death-bed whereon it must expire, Consumed with that which it was nourishd by. It shall be cause of war and dire events, And set dissension twixt the son and sire; Subject and servile to all discontents, As dry combustious matter is to fire: Sith in his prime Death doth my love destroy, They that love best their loves shall not enjoy",
+  ];
+
+  return text;
+};
+
+export { getShakespeare };
