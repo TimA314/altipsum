1173f082093868d429113eff2dfe5a2c5e177741
TimA314 <ci0d7j9m@mailer.me>
2021-12-29 15:31:51 -0800

    add content to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <p>
+        Generate Lorem Ipsum for projects from Shakespeare to the latest news.
+      </p>
     </div>
   );
 }
